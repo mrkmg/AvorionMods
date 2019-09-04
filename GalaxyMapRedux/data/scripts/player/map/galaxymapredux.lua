31990f836f71f6d57736e383349056f2dddec423
Kevin Gravier <kevin@mrkmg.com>
2019-09-04 10:12:21 -0400

    started galaxy map redux

---
+++ GalaxyMapRedux/data/scripts/player/map/galaxymapredux.lua
@@ -0,0 +1,28 @@
+-- GalaxyMapRedux
+-- MrKMG (Kevin Gravier)
+-- MIT License 2019
+
+--namespace GalaxyMapRedux
+GalaxyMapRedux = {}
+GalaxyMapRedux.ShipListWindow = nil
+GalaxyMapRedux.OrdersWindow = nil
+
+if onClient() then
+    -- Client Start
+
+    function GalaxyMapRedux.initalize()
+
+    end
+
+    -- Client End
+end
+
+if onServer() then
+    -- Server Start
+
+    -- Server End
+end
+
+-- Shared Start
+
+-- Shared End
