2d1dc707967a5af2d1723a4d3152e5e6104cade9
Kevin Gravier <kevin@mrkmg.com>
2019-09-16 20:21:49 -0400

    Fixed bug with closing distance to target

--- TransferCargoOrder/data/scripts/entity/ai/transferCargo.lua
+++ TransferCargoOrder/data/scripts/entity/ai/transferCargo.lua
@@ -4,28 +4,39 @@
 
 package.path = package.path .. ";data/scripts/lib/?.lua"
 
-include ("stringutility")
-include ("refineutility")
-include ("utility")
+include("stringutility")
+include("refineutility")
+include("utility")
 
 --namespace TransferCargo
 TransferCargo = {}
 
 local targetEntity
 local didInit = false
+local States = {
+    Init = 1,
+    Flying = 2,
+    Closing = 3,
+    Transferring = 4,
+    Stuck = 5,
+    Finished = 6,
+}
+local state = States.Init
 
 function TransferCargo.initialize(targetIndex)
     if targetIndex == nil then return end
     targetEntity = Entity(targetIndex)
     didInit = true
-    
+
     if not valid(targetEntity) then
-      terminate()
+        terminate()
     end
+
+    TransferCargo.updateServer()
 end
 
 function TransferCargo.getUpdateInterval()
-    return 1
+    return 5
 end
 
 function TransferCargo.announceError(chatMessage, alertMessage)
@@ -49,59 +60,129 @@ function TransferCargo.hasCaptain()
     return true
 end
 
-function TransferCargo.updateServer(timeStep)
-    if didInit == false then return end
-    
-    if not TransferCargo.hasCaptain() then
-        ShipAI():setPassive()
-        terminate()
+function TransferCargo.setState()
+    local dist = Entity():getNearestDistance(targetEntity)
+
+    if dist < 50 then
+        state = States.Transferring
         return
     end
 
-    if not valid(targetEntity) then
-      TransferCargo.announceError("Sir, we can not find the target craft in %s"%_T, "Your craft in %s can not find their target to transfer their cargo"%_T)
-        ShipAI():setPassive()
-        terminate()
-      return
-    end
-    
-    local entity = Entity()
-    
-    if entity.maxCargoSpace - entity.freeCargoSpace < 1 then
-      ShipAI():setStatus("Idle", {})
-      ShipAI():setPassive()
-      terminate()
-    end
-    
-    if targetEntity.freeCargoSpace < 1 then
-      TransferCargo.announceError("Sir, there is no free space on the target in %s"%_T, "Your craft in %s can not transfer their cargo because our target has no cargo space left"%_T)
-      ShipAI():setStatus("Idle", {})
-      ShipAI():setPassive()
-      terminate()
-      return
-    end
-    
-    if entity:getNearestDistance(targetEntity) > 50 then
-      ShipAI():setStatus("Flying to ${name} /* ship AI status*/"%_T%{name = targetEntity.name}, {})
-      ShipAI():setFly(targetEntity.translationf, 0)
-      return 
-    end
-    
-    ShipAI():setStatus("Transferring Cargo to ${name} /* ship AI status*/"%_T%{name = targetEntity.name}, {})
-    ShipAI():setPassive()
-    
-    local cargos = entity:getCargos()
-    
-    for good,count in pairs(cargos) do
-      local transferVolume = math.min(count * good.size, targetEntity.freeCargoSpace)
-      local transferAmount = math.floor(transferVolume / good.size)
-      if transferAmount < 1 then
+    if state == States.Init then
+        state = States.Flying
         return
-      end
-      
-      targetEntity:addCargo(good, transferAmount)
-      entity:removeCargo(good, transferAmount)
     end
+
+    if state ~= States.Flying and dist > 500 then
+        state = States.Init
+        return
+    end
+
+    local ai = ShipAI();
+
+    if state == States.Flying and ai.isStuck then
+        if dist < 500 then
+            state = States.Closing
+        else
+            state = States.Stuck
+        end
+        return
+    end
+
+    if state == States.Closing and ai.isStuck then
+        state = States.Stuck
+        return
+    end
+end
+
+function TransferCargo.run()
+    local ai = ShipAI()
+    local entity = Entity()
+
+    if state == States.Flying then
+        ai:setStatus("Flying to ${name} /* ship AI status*/" % _T % { name = targetEntity.name }, {})
+        ai:setFly(targetEntity.translationf, 0)
+    end
+
+    if state == States.Closing then
+        ai:setStatus("Closing distance to ${name} /* ship AI status*/" % _T % { name = targetEntity.name }, {})
+        local targetPos = (entity.translationf + targetEntity.translationf) / 2
+        ai:setFlyLinear(targetPos, 0, false)
+    end
+
+    if state == States.Transferring then
+        ai:setStatus("Transferring Cargo to ${name} /* ship AI status*/" % _T % { name = targetEntity.name }, {})
+        ai:setPassive()
+
+        local cargos = entity:getCargos()
+
+        for good, count in pairs(cargos) do
+            local transferVolume = math.min(count * good.size, targetEntity.freeCargoSpace)
+            local transferAmount = math.floor(transferVolume / good.size)
+            if transferAmount < 1 then
+                return
+            end
+
+            targetEntity:addCargo(good, transferAmount)
+            entity:removeCargo(good, transferAmount)
+        end
+        state = States.Finished
+    end
+end
+
+function TransferCargo.checkQuitConditions()
+    local ai = ShipAI()
+
+    if not TransferCargo.hasCaptain() then
+        ai:setPassive()
+        return true
+    end
+
+    if not valid(targetEntity) then
+        TransferCargo.announceError("Sir, we can not find the target craft in %s" % _T, "Your craft in %s can not find their target to transfer their cargo" % _T)
+        ai:setStatus("Idle", {})
+        ai:setPassive()
+        return true
+    end
+
+    local entity = Entity()
+
+    if entity.maxCargoSpace - entity.freeCargoSpace < 1 then
+        ai:setStatus("Idle", {})
+        ai:setPassive()
+        return true
+    end
+
+    if targetEntity.freeCargoSpace < 1 then
+        TransferCargo.announceError("Sir, there is no free space on the target in %s" % _T, "No cargo space to transfer to in %s." % _T)
+        ai:setStatus("Idle", {})
+        ai:setPassive()
+        return true
+    end
+
+    if state == States.Stuck then
+        TransferCargo.announceError("Sir, we can not reach the target in %s" % _T, "Your craft in %s can not reach their target" % _T)
+        ai:setStatus("Idle", {})
+        ai:setPassive()
+        return true
+    end
+
+    if state == States.Finished then
+        return true
+    end
+
+    return false
+end
+
+function TransferCargo.updateServer(timeStep)
+    if didInit == false then return end
+
+    if TransferCargo.checkQuitConditions() then
+        terminate()
+    end
+
+    TransferCargo.setState()
+    TransferCargo.run()
 end
 
 
