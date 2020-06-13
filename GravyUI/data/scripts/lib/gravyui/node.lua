26152f840f68e9ab77aa747645ffa398d1c96848
Bubbet <brett1057228@gmail.com>
2020-06-13 11:06:21 -0600

    fixed misunderstanding about multiconstructor and added the behavior i thought was intended

--- GravyUI/data/scripts/lib/gravyui/node.lua
+++ GravyUI/data/scripts/lib/gravyui/node.lua
@@ -31,11 +31,15 @@ GravyUINode.offset = include("gravyui/plugins/offset")
 --[[
     The formatting on this return is very picky or it'll throw eof errors in other mods when adding plugins
     (width: number, height: number)
+    (vec2 constructor)
     (box: Rect)
 ]]
 local call = function(_, a, b)
-    if b == nil then
+    if b ~= nil then
         a = Rect(vec2(0, 0), vec2(a, b))
+    end
+    if a.__avoriontype == 'vec2' then
+        a = Rect(vec2(0), a)
     end
     return GravyUINode:new(a)
 end
