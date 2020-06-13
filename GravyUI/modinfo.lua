b6e066a6db8733996ea0497b6f462c228bb62270
Bubbet <brett1057228@gmail.com>
2020-06-13 08:19:11 -0600

    plugin adding fix for gravyui

--- GravyUI/modinfo.lua
+++ GravyUI/modinfo.lua
@@ -42,7 +42,7 @@ meta =
     --      {id = "OptionalMod", min = "0.2", optional = true}, -- we support OptionalMod optionally, starting at version 0.2
     -- },
     dependencies = {
-        {id = "Avorion", max = "1.0"}
+        {id = "Avorion", max = "1.1"}
     },
 
     -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
